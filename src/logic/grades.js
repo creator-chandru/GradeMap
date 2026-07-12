6cbf71bcf038cb71ddcbf456509dd2b788db56cc
creator-chandru <creatorchandru31@gmail.com>
2026-07-12 09:24:31 +0530

    calculateRequiedMark created, gradeSlab defined

--- src/logic/grades.js
+++ src/logic/grades.js
@@ -1,3 +1,36 @@
+const gradeSlab = [
+    {
+        grade_rep: 'S',
+        gradePoints: 10,
+        minimumMarks: 91
+    },
+    {
+        grade_rep: 'A+',
+        gradePoints: 9,
+        minimumMarks: 81
+    },
+    {
+        grade_rep: 'A',
+        gradePoints: 8,
+        minimumMarks: 71
+    },
+    {
+        grade_rep: 'B+',
+        gradePoints: 7,
+        minimumMarks: 61
+    },
+    {
+        grade_rep: 'B',
+        gradePoints: 6,
+        minimumMarks: 56
+    },
+    {
+        grade_rep: 'C',
+        gradePoints: 5,
+        minimumMarks: 50
+    },
+];
+
 function getGradeFromTotal(total){
     const grades = {
         gradePoints : 0,
@@ -38,4 +71,49 @@ function getGradeFromTotal(total){
     return grades;
 }
 
-console.log(getGradeFromTotal());
+console.log(getGradeFromTotal(100));
+
+function calculateRequiredMark(internal , type){
+    
+    const mixedGrades = gradeSlab.map((grade)=>{
+        const expectedResult = {
+        gradeStatus: '',
+        possibility: '',
+        marksRequired : 0
+        };
+        expectedResult.gradeStatus = grade.grade_rep;
+        const required_endsem_mark = grade.minimumMarks - internal;
+        let endsem_scaled_mark;
+        if(type.toLowerCase() === 't'){
+            endsem_scaled_mark = 60;
+        }
+        else if(type.toLowerCase() === 'i'){
+            endsem_scaled_mark = 50;
+        }
+        else{
+            console.log("Enter valid course type");
+        }
+        const raw_endsem_mark = ((required_endsem_mark/endsem_scaled_mark) * 100);
+        expectedResult.marksRequired = Math.ceil(raw_endsem_mark);
+        if(raw_endsem_mark > 100){
+            expectedResult.possibility = 'Impossible!';
+        }
+        else if(raw_endsem_mark > 80){
+            expectedResult.possibility = 'Achievable with hard work';
+        }
+        else if(raw_endsem_mark > 60){
+            expectedResult.possibility = 'Achievable';
+        }
+        else if(raw_endsem_mark > 45){
+            expectedResult.possibility = 'Easily Achievable';
+        }else{
+            expectedResult.possibility = 'Already Achieved, if you pass the endsemester examinations';
+        }
+        return expectedResult;
+        
+    });
+    
+    return mixedGrades;
+}
+
+console.log(calculateRequiredMark(50,'i'));
