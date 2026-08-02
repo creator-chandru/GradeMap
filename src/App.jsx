b7323ed9a05269048ef2e23d28004700484990c2
creator-chandru <creatorchandru31@gmail.com>
2026-08-02 20:04:17 +0530

    adjusted gap between form and cards

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ export function App() {
   return (
     <>
       <Header />
-      <main className = "flex flex-col md:flex-row md:items-stretch gap-6 md:gap-10 max-w-6xl mx-auto p-4 md:p-8">
+      <main className = "flex flex-col md:flex-row md:items-stretch gap-6 md:gap-12 max-w-6xl mx-auto p-4 md:p-8">
 
           <div className = "w-full md:w-auto md:flex-shrink-0 flex flex-col">
               <SubjectForm subjectsList = {subjectsList} setSubjectsList = {setSubjectsList}/>
